9670fef27890607670688919e82811c09686c6c3
Clarissa Borges <clarissalimab08@gmail.com>
2019-06-25 15:02:51 -0300

    Improves metric_helper methods

--- app/helpers/metric_helper.rb
+++ app/helpers/metric_helper.rb
@@ -34,47 +34,24 @@ module MetricHelper
         date_axis = []
         points_axis = []
         ideal_line = []
-        metric_burndown_array = []
-        amount_of_sprints = release.sprints.count
-        metric_velocity_value = 0
-        planned_points = 0
-        burned_points = 0
 
         velocity = get_sprints_informations(release.sprints, sprint)
         total_points = get_total_points(sprint)
         burned_stories = get_burned_points(sprint, burned_stories)
         total_sprints_points = velocity[:total_points]
         velocities = velocity[:velocities]
-
         range_dates = (sprint.initial_date .. sprint.final_date)
 
         set_dates_and_points(burned_stories, date_axis, points_axis, range_dates, total_points)
-        days_of_sprint = date_axis.length - 1
-        set_ideal_line(days_of_sprint, ideal_line, total_points)
-        ideal_burned_points = ideal_line[0] - ideal_line[1]
+        set_ideal_line(date_axis.length - 1, ideal_line, total_points)
 
-        for i in 0..(date_axis.length - 2)
-          real_burned_points = points_axis[i] - points_axis[i + 1]
-          burned_percentage = Float((real_burned_points).abs * 100) / ideal_burned_points
-          metric_burndown_array.push(burned_percentage)
-        end
-
-        for i in 0..(amount_of_sprints - 1)
-          metric_velocity_value += (total_sprints_points[i] - velocities[i])
-        end
-
-        for i in 0..(release.sprints.length - 1)
-          planned_points += velocity[:total_points][i]
-          burned_points += velocity[:completed_points][i]
-        end
-
-        metric_burndown_value = calculate_burndown(metric_burndown_array)
-
-        metric_debts_value = Float(planned_points - burned_points) / planned_points
-        metric_debts_value = calculate_velocity_and_debt(metric_debts_value)
-
-        total_points = get_total_points_release(release)
-        metric_velocity_value = Float metric_velocity_value / total_points
+        planned_points = calculate_points(release, velocity, total_points)
+        burned_points = calculate_points(release, velocity, completed_points)
+        
+        total_points = get_total_points_release(release) 
+        metric_burndown_value = calculate_burndown(date_axis, points_axis, (ideal_line[0] - ideal_line[1]))
+        metric_debts_value = calculate_velocity_and_debt(Float(planned_points - burned_points) / planned_points)
+        metric_velocity_value = calculate_metric_velocity_value(release.sprints.count, total_sprints_points, velocities, total_points)
         metric_velocity_value = calculate_velocity_and_debt(metric_velocity_value)
 
         return metrics = { metric_debts_value: metric_debts_value,
@@ -102,23 +79,54 @@ module MetricHelper
     return values
   end
 
-  def calculate_burndown(metric)
+  def calculate_burndown(date_axis, points_axis, ideal_burned_points)
+    for i in 0..(date_axis.length - 2)
+      real_burned_points = points_axis[i] - points_axis[i + 1]
+      burned_percentage = Float((real_burned_points).abs * 100) / ideal_burned_points
+      metric_burndown_array.push(burned_percentage)
+    end
+
     values = 0
 
-    for i in 0..(metric.length - 1)
-      if metric[i] <= 10 || metric[i] >= 200
+    for i in 0..(metric_burndown_array.length - 1)
+      if metric_burndown_array[i] <= 10 || metric_burndown_array[i] >= 200
         values += 0
-      elsif metric[i] <= 40
+      elsif metric_burndown_array[i] <= 40
         values += 1
-      elsif metric[i] <= 60
+      elsif metric_burndown_array[i] <= 60
         values += 2
-      elsif metric[i] <= 80
+      elsif metric_burndown_array[i] <= 80
         values += 3
-      elsif metric[i] <= 100
+      elsif metric_burndown_array[i] <= 100
         values += 4
       end
     end
 
-    return Float values / metric.length
+    return Float values / metric_burndown_array.length
+  end
+
+  def calculate_metric_burndown_array(date_axis, points_axis, ideal_burned_points)
+    for i in 0..(date_axis.length - 2)
+      real_burned_points = points_axis[i] - points_axis[i + 1]
+      burned_percentage = Float((real_burned_points).abs * 100) / ideal_burned_points
+      metric_burndown_array.push(burned_percentage)
+    end
+    return metric_burndown_array
+  end
+
+  def calculate_metric_velocity_value(amount_of_sprints, total_sprints_points, velocities, total_points)
+    metric_velocity_value = 0
+    for i in 0..(amount_of_sprints - 1)
+      metric_velocity_value += (total_sprints_points[i] - velocities[i])
+    end
+    return Float metric_velocity_value / total_points
+  end
+
+  def calculate_points(release, velocity, range)
+    points = 0
+    for i in 0..(release.sprints.length - 1)
+      points += velocity[:range][i]
+    end
+    return points
   end
 end
